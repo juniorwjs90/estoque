787cd0eba9c0ab0c046781cbe45e859ca2c7f190
wellington <junior_wjs@hotmail.com>
2023-10-25 22:00:04 -0300

    Inclusão dos métodos de busca unidade de medida por id, editar e apagar unidade de medida.

--- src/main/java/com/praticaprofissional/estoque/controller/UnidadeMedidaController.java
+++ src/main/java/com/praticaprofissional/estoque/controller/UnidadeMedidaController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -16,7 +17,7 @@ public class UnidadeMedidaController {
 
     @PostMapping
     public UnidadeMedida salvar(@RequestBody UnidadeMedida addUnidadeMedida){
-        return service.salvarUnidadeMedida(addUnidadeMedida);
+        return service.adicionarUnidadeMedida(addUnidadeMedida);
     }
 
     @GetMapping
@@ -24,4 +25,18 @@ public class UnidadeMedidaController {
         return service.listarUnidadeMedida();
     }
 
+    @GetMapping("/{id}")
+    public Optional<UnidadeMedida> buscarUnidadeMedidaPorId(@PathVariable(name = "id") Long unidadeMedidaId){
+        return service.buscarPorId(unidadeMedidaId);
+    }
+
+    @PutMapping("/{id}")
+    public UnidadeMedida editarUnidadeMedida(@PathVariable(name = "id") Long unidadeMedidaId, @RequestBody UnidadeMedida unidadeMedidaEditar){
+        return service.editarUnidadeMedida(unidadeMedidaId, unidadeMedidaEditar);
+    }
+
+    @DeleteMapping("/{id}")
+    public String apagarUnidadeMedidaPorId(@PathVariable(name = "id") Long unidadeMedidaId){
+        return service.apagarUnidadeMedida(unidadeMedidaId);
+    }
 }
