d878f1cc973d88c4f72e6988d3adf6ed710b53d4
wellington <junior_wjs@hotmail.com>
2023-10-30 23:23:54 -0300

    Adição das classes controller e service, interface repository do Produto.

---
+++ src/main/java/com/praticaprofissional/estoque/controller/ProdutoController.java
@@ -0,0 +1,34 @@
+package com.praticaprofissional.estoque.controller;
+
+import com.praticaprofissional.estoque.model.Produto;
+import com.praticaprofissional.estoque.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    private final ProdutoService service;
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto addProduto){
+        return service.adicionarProduto(addProduto);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> buscarProdutoPorId(@PathVariable(name = "id") Long produtoId){
+        return service.buscarProdutoPorId(produtoId);
+    }
+    @PutMapping("/{id}")
+    public Produto editarProduto(@PathVariable(name = "id") Long produtoId, @RequestBody Produto produtoParaEditar){
+        return service.editarProduto(produtoId, produtoParaEditar);
+    }
+    @DeleteMapping("/{id}")
+    public String apagarProdutoPorId(@PathVariable(name = "id") Long produtoId){
+        return service.apagarProduto(produtoId);
+    }
+}
