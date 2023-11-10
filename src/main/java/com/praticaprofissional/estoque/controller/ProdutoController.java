cc614a050ab8170bd4664a8747ca74e8aff8f255
wellington <junior_wjs@hotmail.com>
2023-11-10 00:09:25 -0300

    Implementação do método para listar produtos. Inlusão da anotação @CrossOrigin que viabiliza o funcionamento do método na página html.

--- src/main/java/com/praticaprofissional/estoque/controller/ProdutoController.java
+++ src/main/java/com/praticaprofissional/estoque/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import com.praticaprofissional.estoque.service.ProdutoService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,14 +21,22 @@ public class ProdutoController {
         return service.adicionarProduto(addProduto);
     }
 
+    @CrossOrigin(origins = "*")
+    @GetMapping
+    public List<Produto> consultarProduto(){
+        return service.listarProduto();
+    }
+
     @GetMapping("/{id}")
     public Optional<Produto> buscarProdutoPorId(@PathVariable(name = "id") Long produtoId){
         return service.buscarProdutoPorId(produtoId);
     }
+
     @PutMapping("/{id}")
     public Produto editarProduto(@PathVariable(name = "id") Long produtoId, @RequestBody Produto produtoParaEditar){
         return service.editarProduto(produtoId, produtoParaEditar);
     }
+
     @DeleteMapping("/{id}")
     public String apagarProdutoPorId(@PathVariable(name = "id") Long produtoId){
         return service.apagarProduto(produtoId);
