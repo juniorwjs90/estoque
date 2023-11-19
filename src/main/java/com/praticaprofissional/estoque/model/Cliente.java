f1f2222f69095a1c7bc1180ac2581adb9b8584ea
wellington <junior_wjs@hotmail.com>
2023-11-19 16:33:06 -0300

    Cliente model, service, controller, repository.

---
+++ src/main/java/com/praticaprofissional/estoque/model/Cliente.java
@@ -0,0 +1,129 @@
+package com.praticaprofissional.estoque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String cep;
+    private int numero;
+    private int numero_telefone_fixo;
+    private int numero_telefone_celular;
+    private String endereco;
+    private String bairro;
+    private String estado;
+    private String municipio;
+    private String complemento;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero_telefone_fixo() {
+        return numero_telefone_fixo;
+    }
+
+    public void setNumero_telefone_fixo(int numero_telefone_fixo) {
+        this.numero_telefone_fixo = numero_telefone_fixo;
+    }
+
+    public int getNumero_telefone_celular() {
+        return numero_telefone_celular;
+    }
+
+    public void setNumero_telefone_celular(int numero_telefone_celular) {
+        this.numero_telefone_celular = numero_telefone_celular;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
